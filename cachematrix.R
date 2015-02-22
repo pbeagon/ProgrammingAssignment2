a900ed0186168c361e94f2c9842eb45ae0885a3c
pbeagon <pbeagon@gmail.com>
2015-02-22 15:47:25 +0000

    Commit cachematrix.R for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: Creates a list of nested functions performed on a matrix 'x' 
+## These nested functions "set" (or cache) and "get" the matrix and its inverse
 
-## Write a short comment describing this function
+## cacheSolve: Return the inverse of matrix 'x' from cache if possible
 
 makeCacheMatrix <- function(x = matrix()) {
     inv  <- NULL
+    
+    # superassign the passed variable 'y' to the local variable 'x'
+    # superassign the local variable 'inv' to NULL   
     set <- function(y) {
       x <<- y
       inv <<- NULL
     }
     get <- function() x
-    setinv <- function(solve) inv <<- solve
+    
+    # superassign the passed variable 'inv_passed' to the local variable 'inv'
+    setinv <- function(inv_passed) inv <<- inv_passed
+    
+    # Get the cached value of matrix inverse 'inv'
     getinv <- function() inv
+    
+    # Store nested functions in a list
     list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
 }
 
 
-## Return a matrix that is the inverse of 'x'
+## cacheSolve returns the matrix 'x' inverse from cache or by calculation
+## Where the matrix 'x' inverse value is calculated, it is then cached
 
 cacheSolve <- function(x, ...) {
+  
+    # Get the cached value of matrix x, and if not a NULL value, return it
     inv <- x$getinv()
     if(!is.null(inv)){
       message("Getting cached data")
       return(inv)        
     }
+    
+    # If the cached value of matrix 'x' is null, calculate it
     message("Calculating matrix inverse")
     matrix_x  <- x$get()
     inv  <- solve(matrix_x)
+    
+    # Set the inverse of the matrix 'x' in the cache list
     x$setinv(inv)
+    
+    # Display the newly calculated inverse of the matrix 'x' 
     inv
 }
